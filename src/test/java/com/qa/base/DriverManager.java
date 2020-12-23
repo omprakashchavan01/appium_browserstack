556ea128da696fe4ee78a43d22208904e93fb53d
Omprakash Chavan <askomdch@gmail.com>
2020-12-23 11:07:46 +0530

    Revert to skeleton rep

--- src/test/java/com/qa/base/DriverManager.java
+++ src/test/java/com/qa/base/DriverManager.java
@@ -1,14 +1,6 @@
 package com.qa.base;
 
-import com.qa.utils.HttpHelper;
-import com.qa.utils.JsonParser;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
-import org.json.JSONObject;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.URL;
 
 public class DriverManager {
     private static final ThreadLocal<AppiumDriver> driver = new ThreadLocal<>();
@@ -22,41 +14,6 @@ public class DriverManager {
     }
 
     public static void initializeDriver(String deviceID) throws Exception {
-        AppiumDriver driver;
-    //    String accessKey = "WSh657ymwgyZTQkTtVoy";
-    //    String userName = "omprakashchavan1";
-        String userName = System.getenv("BROWSERSTACK_USERNAME");
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        String browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
-        String buildName = System.getenv("BROWSERSTACK_BUILD_NAME");
-        String browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
-        String app = System.getenv("BROWSERSTACK_APP_ID");
-
-        JSONObject deviceObj = JsonParser.parse("Devices.json").getJSONObject(deviceID);
-
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("device", deviceObj.getString("device"));
-        caps.setCapability("os_version", deviceObj.getString("os_version"));
-        caps.setCapability("project", "My First Project");
-        caps.setCapability("build", buildName);
-        caps.setCapability("name", "Bstack-[Java] Sample Test");
-
-     //   JSONObject appUrlObj = new JSONObject(HttpHelper.post());
-
-        caps.setCapability("app", app);
-
-        URL url = new URL("https://" + userName + ":" + accessKey + "@hub-cloud.browserstack.com/wd/hub");
-
-        switch (deviceID) {
-            case "1":
-                driver = new AndroidDriver(url, caps);
-                break;
-            case "2":
-                driver = new IOSDriver(url, caps);
-                break;
-            default:
-                throw new Exception("Invalid device ID! - " + deviceID);
-        }
-        setDriver(driver);
+        
     }
 }
