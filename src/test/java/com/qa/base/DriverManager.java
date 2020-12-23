3e9636c512d7a1efa110c480a8ec149fa1367aea
Omprakash Chavan <askomdch@gmail.com>
2020-12-23 16:38:59 +0530

    changes

--- src/test/java/com/qa/base/DriverManager.java
+++ src/test/java/com/qa/base/DriverManager.java
@@ -1,6 +1,13 @@
 package com.qa.base;
 
+import com.qa.utils.JsonParser;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.json.JSONObject;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
 
 public class DriverManager {
     private static final ThreadLocal<AppiumDriver> driver = new ThreadLocal<>();
@@ -14,6 +21,32 @@ public class DriverManager {
     }
 
     public static void initializeDriver(String deviceID) throws Exception {
-        
+        AppiumDriver driver;
+        String userName = "omprakashchavan1";
+        String accessKey = "WSh657ymwgyZTQkTtVoy";
+
+        JSONObject deviceObj = new JSONObject(JsonParser.parse("Devices.json").getJSONObject(deviceID).toString());
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability("device", deviceObj.getString("device"));
+        caps.setCapability("os_version", deviceObj.getString("os_version"));
+        caps.setCapability("project", "My First Project");
+        caps.setCapability("build", "My First Build");
+        caps.setCapability("name", "Bstack-[Java] Sample Test");
+        caps.setCapability("app", deviceObj.getString("app_url"));
+
+        URL url = new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub");
+
+        switch (deviceID){
+            case "1":
+                driver = new AndroidDriver(url, caps);
+                break;
+            case "2":
+                driver = new IOSDriver(url, caps);
+                break;
+            default:
+                throw new IllegalStateException("invalid device id" + deviceID);
+        }
+        setDriver(driver);
     }
 }
